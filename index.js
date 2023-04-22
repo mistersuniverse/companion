e3d2ea9a33cbfa1c02e417c946b3fbe1910e9c2f
surajshukla <surajshukla5604@gmail.com>
2023-04-22 22:41:13 +0530

    created totally new UI design

--- index.js
+++
@@ -1,7 +0,0 @@
-function loadNavbar() {
-    fetch('navbar.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('navbar').innerHTML = data;
-      });
-  }
