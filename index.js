8702afdb8a8f1ca6c608c73540744d1ea0561d9e
surajshukla <surajshukla5604@gmail.com>
2023-04-22 10:19:19 +0530

    created basic structure for production

---
+++ index.js
@@ -0,0 +1,7 @@
+function loadNavbar() {
+    fetch('navbar.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('navbar').innerHTML = data;
+      });
+  }
