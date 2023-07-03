842bc4e56e75f01f509f37bf6e5c728844134d9f
surajshukla <surajshukla5604@gmail.com>
2023-07-03 23:17:29 +0530

    updated wrong link of cost and management pyq

--- src/Courses/BMS/SEM2/COSTANDMANAGEMENTACCOUTING/pyqs.js
+++ src/Courses/BMS/SEM2/COSTANDMANAGEMENTACCOUTING/pyqs.js
@@ -1,3 +1,3 @@
 export default [
-    {link:"https://drive.google.com/drive/folders/1On6aTBqfXYDa7ix4XX2StQMKTCoqTouW?usp=share_link", title:"Open"}
+    {link:"https://drive.google.com/drive/folders/15QlcZwk2Bs8qbqhEdQADM1PUtDgpECQs", title:"Open"}
 ]
